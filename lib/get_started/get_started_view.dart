531a9e350419cfe22e0a610f544dc0b16aee50e6
Dmytro Yakubenko <dmytro.yakubenko@lnoks.com>
2024-08-05 12:55:32 +0300

    update:make text and images adaptive

--- lib/get_started/get_started_view.dart
+++ lib/get_started/get_started_view.dart
@@ -1,52 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_multi_app/utils/custom_background.dart';
+import 'package:flutter_multi_app/utils/text_scale_size.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class GetStartedView extends StatelessWidget {
   const GetStartedView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       backgroundColor: const Color(0xFFf2e9e0),
       body: CustomPaint(
-        size: const Size(double.infinity, double.infinity),
+        size: Size(screenWidth, screenHeight),
         painter: CustomBackgroundPainter(),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Stack(
-            children: [
-              Positioned(top: 0,right: -24,
-                  child: Image.asset('assets/images/tarelka-removebg-preview.png',height: 300,width: 300,fit: BoxFit.cover,)),
-              Positioned(top: 160,left: 70,
-                  child: Image.asset('assets/images/mint_leaf-removebg-preview.png',height: 64,width: 64,)),
-              Positioned(top: 230,left: 140,child: Image.asset('assets/images/mint_leaf-removebg-preview.png',height: 50,width: 50,)),
-              Positioned(top: 235,left: 80,child: Image.asset('assets/images/strawberry2-removebg-preview.png',height: 50,width: 50,)),
-              Column(
+        child: Stack(
+          children: [
+            Positioned(
+              top: screenHeight * -0.05,
+              right: screenWidth * 0.05,
+              child: Image.asset(
+                'assets/images/tarelka-removebg-preview.png',
+                height: screenHeight * 0.4,
+                width: screenWidth * 0.5,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned(
+              top: screenHeight * 0.2,
+              left: screenWidth * 0.2,
+              child: Image.asset(
+                'assets/images/mint_leaf-removebg-preview.png',
+                height: screenHeight * 0.08,
+                width: screenWidth * 0.15,
+              ),
+            ),
+            Positioned(
+              top: screenHeight * 0.27,
+              left: screenWidth * 0.42,
+              child: Image.asset(
+                'assets/images/mint_leaf-removebg-preview.png',
+                height: screenHeight * 0.07,
+                width: screenWidth * 0.13,
+              ),
+            ),
+            Positioned(
+              top: screenHeight * 0.27,
+              left: screenWidth * 0.22,
+              child: Image.asset(
+                'assets/images/strawberry2-removebg-preview.png',
+                height: screenHeight * 0.07,
+                width: screenWidth * 0.13,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
+              child: Column(
                 children: [
                   const Spacer(flex: 4),
-                  const Align(
+                   Align(
                     alignment: Alignment.topLeft,
                     child: Padding(
-                      padding: EdgeInsets.only(left: 28.0),
+                      padding: const EdgeInsets.only(left: 20.0),
                       child: Text(
                         'Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit sed.',
-                        style: TextStyle(
-                            fontSize: 18,
+                        textScaler: TextScaler.linear(ScaleSize.textScaleFactor(context)),
+                        style:  TextStyle(
                             color: Colors.brown,
+                            fontSize: ScreenUtil().setSp(16),
                             fontWeight: FontWeight.bold),
                       ),
                     ),
                   ),
                   const SizedBox(height: 20),
-                  const Align(
+                   Align(
                     alignment: Alignment.topLeft,
                     child: Padding(
-                      padding: EdgeInsets.only(left: 28.0),
+                      padding: EdgeInsets.only(left: 20.0),
                       child: Text(
                         'Tempor incididunt ut labore et \ndolore magna aliqua consectetur \nadipiscing elit sed.',
                         style: TextStyle(
-                          fontSize: 18,
                           color: Colors.brown,
+                          fontSize: ScreenUtil().setSp(16),
                         ),
+                        textScaler: TextScaler.linear(ScaleSize.textScaleFactor(context)),
                       ),
                     ),
                   ),
@@ -56,56 +95,27 @@ class GetStartedView extends StatelessWidget {
                     child: ElevatedButton(
                       onPressed: () {},
                       style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
+                        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 40.0),
                         foregroundColor: const Color(0xFFf3e1d4),
                         backgroundColor: const Color(0xFFf3e1d4),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(30),
                         ),
                       ),
-                      child: const Text('Get Started',style: TextStyle(color: Color(0xFFaa9184),fontSize: 18),),
+                      child: Text(
+                        'Get Started',
+                        textScaler: TextScaler.linear(ScaleSize.textScaleFactor(context)),
+                        style: TextStyle(color: Colors.brown,fontSize:ScreenUtil().setSp(16)),
+                      ),
                     ),
                   ),
-                  const SizedBox(height: 60),
+                  SizedBox(height: screenHeight * 0.05),
                 ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
-}
-
-class CustomBackgroundPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()..color = Colors.brown;
-
-    final path = Path();
-    //Upper part of background
-    paint.color = const Color(0xFF7c453e);
-    path.moveTo(0, size.height * 0.5);
-    path.quadraticBezierTo(size.width * 0.15, size.height * 0.3,
-        size.width * 0.55, size.height * 0.25);
-    path.quadraticBezierTo(size.width * 0.95, size.height * 0.2, size.width, 0);
-    path.lineTo(size.width, 0);
-    path.lineTo(0, 0);
-    path.close();
-
-    //Bottom part of background
-    path.moveTo(size.width, size.height * 0.5);
-    path.quadraticBezierTo(size.width * 0.85, size.height * 0.7,
-        size.width * 0.45, size.height * 0.75);
-    path.quadraticBezierTo(
-        size.width * 0.05, size.height * 0.8, 0, size.height);
-    path.lineTo(0, size.height);
-    path.lineTo(size.width, size.height);
-    path.close();
-
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_multi_app/get_started/get_started_view.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async{
+  await ScreenUtil.ensureScreenSize();
   runApp(const MyApp());
 }
 
@@ -11,14 +13,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ScreenUtilInit(
+        minTextAdapt: true,
+        splitScreenMode: true,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const GetStartedView()
       ),
-      home: const GetStartedView()
     );
   }
 }
