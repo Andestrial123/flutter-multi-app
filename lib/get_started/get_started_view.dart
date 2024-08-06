690d44ff29332f05e4b5bbd474f23325f630906c
Artem Prysich <a.prysich@foodtech.team>
2024-08-06 23:18:13 +0300

    feature(get_started_view) : fix magic numbers , fix responsive ui

--- lib/get_started/get_started_view.dart
+++ lib/get_started/get_started_view.dart
@@ -7,111 +7,143 @@ class GetStartedView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
-    final screenWidth = MediaQuery.of(context).size.width;
+    final screenUtil = ScreenUtil();
 
     return Scaffold(
       backgroundColor: const Color(0xFFf2e9e0),
       body: CustomPaint(
-        size: Size(screenWidth, screenHeight),
         painter: CustomBackgroundPainter(),
-        child: Stack(
-          children: [
-            Positioned(
-              top: screenHeight * -0.04,
-              right: screenHeight * 0,
-              child: SizedBox(
-                height: screenHeight * 0.5,
-                width: screenWidth * 0.7,
-                child: Image.asset(
-                  'assets/images/tarelka-removebg-preview.png',
-                  fit: BoxFit.cover,
+        child: SafeArea(
+          child: Stack(
+            children: [
+              Positioned(
+                top: -screenUtil.setHeight(50),
+                right: -screenUtil.setWidth(50),
+
+                ///TODO move to const @Lalcovich
+                child: Image.asset('assets/images/tarelka-removebg-preview.png',
+                    fit: BoxFit.cover,
+                    height: screenUtil.setHeight(300),
+                    width: screenUtil.setWidth(300)),
+              ),
+              Positioned(
+                top: 0,
+                child: Container(
+                  constraints: BoxConstraints(
+                      maxHeight: screenUtil.setHeight(230),
+                      maxWidth: screenUtil.screenWidth),
+                  child: Row(
+                    children: [
+                      const Spacer(
+                        flex: 4,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ///TODO move to const @Lalcovich
+                          Image.asset(
+                            'assets/images/mint_leaf-removebg-preview.png',
+                            height: screenUtil.setHeight(50),
+                            width: screenUtil.setWidth(50),
+                          ),
+                          SizedBox(
+                            height: screenUtil.setHeight(6),
+                          ),
+
+                          ///TODO move to const @Lalcovich
+                          Image.asset(
+                            'assets/images/strawberry2-removebg-preview.png',
+                            height: screenUtil.setHeight(50),
+                            width: screenUtil.setWidth(50),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: screenUtil.setWidth(20),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+
+                        ///TODO move to const @Lalcovich
+                        child: Image.asset(
+                          'assets/images/mint_leaf-removebg-preview.png',
+                          height: screenUtil.setHeight(50),
+                          width: screenUtil.setWidth(50),
+                        ),
+                      ),
+                      const Spacer(
+                        flex: 7,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Positioned(
-              top: screenHeight * 0.2,
-              left: screenWidth * 0.13,
-              child: Image.asset(
-                'assets/images/mint_leaf-removebg-preview.png',
-                height: screenHeight * 0.08,
-                width: screenWidth * 0.15,
-              ),
-            ),
-            Positioned(
-              top: screenHeight * 0.33,
-              left: screenWidth * 0.35,
-              child: Image.asset(
-                'assets/images/mint_leaf-removebg-preview.png',
-                height: screenHeight * 0.07,
-                width: screenWidth * 0.13,
-              ),
-            ),
-            Positioned(
-              top: screenHeight * 0.33,
-              left: screenWidth * 0.13,
-              child: Image.asset(
-                'assets/images/strawberry2-removebg-preview.png',
-                height: screenHeight * 0.07,
-                width: screenWidth * 0.13,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
-              child: Column(
-                children: [
-                  const Spacer(flex: 4),
-                   Align(
-                    alignment: Alignment.topLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20.0),
-                      child: Text(
-                        'Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit sed.',
-                        style:  TextStyle(
+              Padding(
+                padding: const EdgeInsets.only(left: 16, bottom: 20, right: 16),
+                child: Column(
+                  children: [
+                    const Spacer(flex: 4),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+
+                        ///TODO move to localization file @Lalcovich
+                        child: Text(
+                          'Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit sed.',
+                          style: TextStyle(
+                              color: Colors.brown,
+                              fontSize: ScreenUtil().setSp(16),
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+
+                        ///TODO move to localization file @Lalcovich
+                        child: Text(
+                          'Tempor incididunt ut labore et \ndolore magna aliqua consectetur \nadipiscing elit sed.',
+                          style: TextStyle(
                             color: Colors.brown,
                             fontSize: ScreenUtil().setSp(16),
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                   Align(
-                    alignment: Alignment.topLeft,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 20.0),
-                      child: Text(
-                        'Tempor incididunt ut labore et \ndolore magna aliqua consectetur \nadipiscing elit sed.',
-                        style: TextStyle(
-                          color: Colors.brown,
-                          fontSize: ScreenUtil().setSp(16),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  const Spacer(flex: 2),
-                  Align(
-                    alignment: Alignment.center,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 40.0),
-                        foregroundColor: const Color(0xFFf3e1d4),
-                        backgroundColor: const Color(0xFFf3e1d4),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
+                    const Spacer(flex: 2),
+                    Align(
+                      alignment: Alignment.center,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 16.0, horizontal: 40.0),
+                          foregroundColor: const Color(0xFFf3e1d4),
+                          backgroundColor: const Color(0xFFf3e1d4),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+
+                        ///TODO move to localization file @Lalcovich
+                        child: Text(
+                          'Get Started',
+                          style: TextStyle(
+                              color: Colors.brown,
+                              fontWeight: FontWeight.w400,
+                              fontSize: screenUtil.setSp(16)),
                         ),
                       ),
-                      child: Text(
-                        'Get Started',
-                        style: TextStyle(color: Colors.brown,fontWeight: FontWeight.w400,fontSize:ScreenUtil().setSp(16)),
-                      ),
                     ),
-                  ),
-                  SizedBox(height: screenHeight * 0.05),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
