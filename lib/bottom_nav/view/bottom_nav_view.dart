e3b892a70e0753569b2f4a6f59b8e5ca21468189
Dmytro Yakubenko <dmytro.yakubenko@lnoks.com>
2024-08-07 21:55:07 +0300

    fix:add packages

---
+++ lib/bottom_nav/view/bottom_nav_view.dart
@@ -0,0 +1,17 @@
+import 'package:auto_route/annotations.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class BottomNavView extends StatefulWidget {
+  const BottomNavView({super.key});
+
+  @override
+  State<BottomNavView> createState() => _BottomNavViewState();
+}
+
+class _BottomNavViewState extends State<BottomNavView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
