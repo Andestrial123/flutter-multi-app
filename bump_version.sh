29b439343a0db7ca98f1a495d23d9795e4ed68f8
Artem Prysich <a.prysich@foodtech.team>
2024-08-09 01:12:57 +0300

    feature(CI-CD-integration) : updating yaml file

--- bump_version.sh
+++ bump_version.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# Функция для увеличения номера сборки
 increment_build_number() {
-  local version_line=$(grep '^version:' pubspec.yaml)
+  local version_line=$(grep "^version:" pubspec.yaml)
   local version=$(echo "$version_line" | awk '{print $2}')
-  local build_number=$(echo "$version" | cut -d '+' -f2)
+  local current_build_number=$(echo "$version" | cut -d '+' -f2)
 
-  local new_build_number=$((build_number + 1))
-  local new_version=$(echo "$version" | sed "s/+$build_number$/+$new_build_number/")
+  local new_build_number=$((current_build_number + 1)) # Это можно увеличить или использовать GITHUB_RUN_NUMBER
 
-  # Обновляем строку версии
+  local new_version="${version%+*}+${GITHUB_RUN_NUMBER}"
+
   sed -i.bak "s/^version:.*/version: $new_version/" pubspec.yaml
 
   echo "Updated pubspec.yaml to version $new_version"
