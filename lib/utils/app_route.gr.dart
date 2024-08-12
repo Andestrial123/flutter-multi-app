2b0e77147b465058bb54a89eef9cc2ef5351aa6e
Dmytro Yakubenko <dmytro.yakubenko@lnoks.com>
2024-08-12 18:26:52 +0300

    feature:update auto route generated file

--- lib/utils/app_route.gr.dart
+++ lib/utils/app_route.gr.dart
@@ -8,20 +8,43 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i7;
-import 'package:flutter_multi_app/features/bottom_nav/view/bottom_nav_view.dart'
+import 'package:auto_route/auto_route.dart' as _i9;
+import 'package:flutter_multi_app/features/auth/presentation/auth_screen.dart'
     as _i1;
-import 'package:flutter_multi_app/features/get_started/get_started_view.dart'
+import 'package:flutter_multi_app/features/bottom_nav/view/bottom_nav_view.dart'
     as _i2;
-import 'package:flutter_multi_app/features/home/home_view.dart' as _i3;
-import 'package:flutter_multi_app/features/location/location_view.dart' as _i4;
-import 'package:flutter_multi_app/features/order/order_view.dart' as _i5;
-import 'package:flutter_multi_app/features/profile/profile_view.dart' as _i6;
+import 'package:flutter_multi_app/features/get_started/get_started_view.dart'
+    as _i3;
+import 'package:flutter_multi_app/features/home/home_view.dart' as _i4;
+import 'package:flutter_multi_app/features/location/location_view.dart' as _i5;
+import 'package:flutter_multi_app/features/main/presentation/main_screen.dart'
+    as _i6;
+import 'package:flutter_multi_app/features/order/order_view.dart' as _i7;
+import 'package:flutter_multi_app/features/profile/profile_view.dart' as _i8;
 
 /// generated route for
-/// [_i1.BottomNavView]
-class BottomNavView extends _i7.PageRouteInfo<void> {
-  const BottomNavView({List<_i7.PageRouteInfo>? children})
+/// [_i1.AuthScreen]
+class AuthRoute extends _i9.PageRouteInfo<void> {
+  const AuthRoute({List<_i9.PageRouteInfo>? children})
+      : super(
+          AuthRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'AuthRoute';
+
+  static _i9.PageInfo page = _i9.PageInfo(
+    name,
+    builder: (data) {
+      return const _i1.AuthScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [_i2.BottomNavView]
+class BottomNavView extends _i9.PageRouteInfo<void> {
+  const BottomNavView({List<_i9.PageRouteInfo>? children})
       : super(
           BottomNavView.name,
           initialChildren: children,
@@ -29,18 +52,18 @@ class BottomNavView extends _i7.PageRouteInfo<void> {
 
   static const String name = 'BottomNavView';
 
-  static _i7.PageInfo page = _i7.PageInfo(
+  static _i9.PageInfo page = _i9.PageInfo(
     name,
     builder: (data) {
-      return const _i1.BottomNavView();
+      return const _i2.BottomNavView();
     },
   );
 }
 
 /// generated route for
-/// [_i2.GetStartedView]
-class GetStartedView extends _i7.PageRouteInfo<void> {
-  const GetStartedView({List<_i7.PageRouteInfo>? children})
+/// [_i3.GetStartedView]
+class GetStartedView extends _i9.PageRouteInfo<void> {
+  const GetStartedView({List<_i9.PageRouteInfo>? children})
       : super(
           GetStartedView.name,
           initialChildren: children,
@@ -48,18 +71,18 @@ class GetStartedView extends _i7.PageRouteInfo<void> {
 
   static const String name = 'GetStartedView';
 
-  static _i7.PageInfo page = _i7.PageInfo(
+  static _i9.PageInfo page = _i9.PageInfo(
     name,
     builder: (data) {
-      return const _i2.GetStartedView();
+      return const _i3.GetStartedView();
     },
   );
 }
 
 /// generated route for
-/// [_i3.HomeView]
-class HomeView extends _i7.PageRouteInfo<void> {
-  const HomeView({List<_i7.PageRouteInfo>? children})
+/// [_i4.HomeView]
+class HomeView extends _i9.PageRouteInfo<void> {
+  const HomeView({List<_i9.PageRouteInfo>? children})
       : super(
           HomeView.name,
           initialChildren: children,
@@ -67,18 +90,18 @@ class HomeView extends _i7.PageRouteInfo<void> {
 
   static const String name = 'HomeView';
 
-  static _i7.PageInfo page = _i7.PageInfo(
+  static _i9.PageInfo page = _i9.PageInfo(
     name,
     builder: (data) {
-      return const _i3.HomeView();
+      return const _i4.HomeView();
     },
   );
 }
 
 /// generated route for
-/// [_i4.LocationView]
-class LocationView extends _i7.PageRouteInfo<void> {
-  const LocationView({List<_i7.PageRouteInfo>? children})
+/// [_i5.LocationView]
+class LocationView extends _i9.PageRouteInfo<void> {
+  const LocationView({List<_i9.PageRouteInfo>? children})
       : super(
           LocationView.name,
           initialChildren: children,
@@ -86,18 +109,37 @@ class LocationView extends _i7.PageRouteInfo<void> {
 
   static const String name = 'LocationView';
 
-  static _i7.PageInfo page = _i7.PageInfo(
+  static _i9.PageInfo page = _i9.PageInfo(
     name,
     builder: (data) {
-      return const _i4.LocationView();
+      return const _i5.LocationView();
     },
   );
 }
 
 /// generated route for
-/// [_i5.OrderView]
-class OrderView extends _i7.PageRouteInfo<void> {
-  const OrderView({List<_i7.PageRouteInfo>? children})
+/// [_i6.MainScreen]
+class MainRoute extends _i9.PageRouteInfo<void> {
+  const MainRoute({List<_i9.PageRouteInfo>? children})
+      : super(
+          MainRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'MainRoute';
+
+  static _i9.PageInfo page = _i9.PageInfo(
+    name,
+    builder: (data) {
+      return const _i6.MainScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [_i7.OrderView]
+class OrderView extends _i9.PageRouteInfo<void> {
+  const OrderView({List<_i9.PageRouteInfo>? children})
       : super(
           OrderView.name,
           initialChildren: children,
@@ -105,18 +147,18 @@ class OrderView extends _i7.PageRouteInfo<void> {
 
   static const String name = 'OrderView';
 
-  static _i7.PageInfo page = _i7.PageInfo(
+  static _i9.PageInfo page = _i9.PageInfo(
     name,
     builder: (data) {
-      return const _i5.OrderView();
+      return const _i7.OrderView();
     },
   );
 }
 
 /// generated route for
-/// [_i6.ProfileView]
-class ProfileView extends _i7.PageRouteInfo<void> {
-  const ProfileView({List<_i7.PageRouteInfo>? children})
+/// [_i8.ProfileView]
+class ProfileView extends _i9.PageRouteInfo<void> {
+  const ProfileView({List<_i9.PageRouteInfo>? children})
       : super(
           ProfileView.name,
           initialChildren: children,
@@ -124,10 +166,10 @@ class ProfileView extends _i7.PageRouteInfo<void> {
 
   static const String name = 'ProfileView';
 
-  static _i7.PageInfo page = _i7.PageInfo(
+  static _i9.PageInfo page = _i9.PageInfo(
     name,
     builder: (data) {
-      return const _i6.ProfileView();
+      return const _i8.ProfileView();
     },
   );
 }
