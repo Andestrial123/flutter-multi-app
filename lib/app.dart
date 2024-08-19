a1ac07fd96d16db3599273a5c1c54e0e75502f80
ivansyvak <vanyafc78@gmail.com>
2024-08-19 18:30:06 +0300

    create animation to hide BottomNav, where ModalGoogleMaps is active

--- lib/app.dart
+++ lib/app.dart
@@ -4,9 +4,7 @@ import 'package:flutter_multi_app/utils/app_route.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_multi_app/di.dart';
 import 'package:flutter_multi_app/features/auth/domain/auth_bloc.dart';
-import 'package:flutter_multi_app/features/get_started/get_started_view.dart';
 import 'package:flutter_multi_app/features/google_maps/domain/google_maps_bloc.dart';
-import 'package:flutter_multi_app/features/main/presentation/main_screen.dart';
 import 'package:flutter_multi_app/features/sign_up/domain/sign_up_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
