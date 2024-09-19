58b1f8f8ddc0bf3918fc8ba9a19e8265a6f3d588
ivansyvak <vanyafc78@gmail.com>
2024-09-19 12:22:03 +0300

    connect dynamic link for Android

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_multi_app/utils/app_route.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,13 +9,34 @@ import 'package:flutter_multi_app/features/sign_up/domain/sign_up_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatefulWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _initDynamicLinks();
+  }
+
+  void _initDynamicLinks() async {
+    FirebaseDynamicLinks.instance.onLink.listen((dynamicLinkData) {
+      final Uri deepLink = dynamicLinkData.link;
+      print('Received deep link: $deepLink');
+    });
+
+    final PendingDynamicLinkData? initialLink =
+    await FirebaseDynamicLinks.instance.getInitialLink();
+
+    if (initialLink != null) {
+      final Uri deepLink = initialLink.link;
+      print('Initial deep link: $deepLink');
+    }
+  }
+
   final _appRouter = AppRouter();
 
   @override
