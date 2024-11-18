6e9d56a89aaae70e224a0bc28f5db03210f08217
Ivan Syvak <Ivan.Syvak@fivesysdev.com>
2024-11-18 14:17:12 +0200

    Change color in bottom bar and add a shadow

--- lib/features/bottom_nav/widgets/custom_bottom_nav_widget.dart
+++ lib/features/bottom_nav/widgets/custom_bottom_nav_widget.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_multi_app/utils/colors.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
   final int currentIndex;
@@ -26,63 +27,78 @@ class CustomBottomNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: const BorderRadius.only(
-        topLeft: Radius.circular(30),
-        topRight: Radius.circular(30),
-      ),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.2),
-              blurRadius: 10,
-              offset: const Offset(0, -2),
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          child: Container(
+            decoration: const BoxDecoration(
+              color: CustomColors.whiteColor,
             ),
-          ],
-        ),
-        padding: EdgeInsets.only(
-            left: paddingHorizontal,
-            right: paddingHorizontal,
-            bottom: 8 + (Platform.isIOS ? 16 : 0),
-            top: 8),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: items.map((item) {
-            int index = items.indexOf(item);
-            return InkWell(
-              onTap: () => onTap(index),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: itemSpacing / 2),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconTheme(
-                      data: IconThemeData(
-                        size: iconSize,
-                        color: currentIndex == index
-                            ? selectedItemColor
-                            : unselectedItemColor,
-                      ),
-                      child: item.icon,
-                    ),
-                    if (item.label != null)
-                      Text(
-                        item.label!,
-                        style: TextStyle(
-                          color: currentIndex == index
-                              ? selectedItemColor
-                              : unselectedItemColor,
+            padding: EdgeInsets.only(
+              left: paddingHorizontal,
+              right: paddingHorizontal,
+              bottom: 8 + (Platform.isIOS ? 16 : 0),
+              top: 8,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: items.map((item) {
+                int index = items.indexOf(item);
+                return InkWell(
+                  onTap: () => onTap(index),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: itemSpacing / 2),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconTheme(
+                          data: IconThemeData(
+                            size: iconSize,
+                            color: currentIndex == index
+                                ? selectedItemColor
+                                : unselectedItemColor,
+                          ),
+                          child: item.icon,
                         ),
-                      ),
-                  ],
-                ),
-              ),
-            );
-          }).toList(),
+                        if (item.label != null)
+                          Text(
+                            item.label!,
+                            style: TextStyle(
+                              color: currentIndex == index
+                                  ? selectedItemColor
+                                  : unselectedItemColor,
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
         ),
-      ),
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            height: 4,
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.6),
+                  blurRadius: 6,
+                  offset: const Offset(0, -4),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
